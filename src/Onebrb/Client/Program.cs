bc0600f46b56bf8d9965d5bee8124f984f27d8f4
Kaloyan Drenski <drenski666@gmail.com>
2020-06-30 15:58:31 +0300

    Get currently logged in user

--- src/Onebrb/Client/Program.cs
+++ src/Onebrb/Client/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Onebrb.Core.Entities;
 
 namespace Onebrb.Client
 {
---
+++ src/Onebrb/Server/Controllers/Api/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Onebrb.Core.Dtos.User;
+using Onebrb.Core.Entities;
+using Onebrb.Server.Utils.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Onebrb.Server.Controllers.Api
+{
+    /// <summary>
+    /// This controller handles requests to api/users
+    /// </summary>
+    [Produces("application/json")]
+    public class UsersController : BaseApiController
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public UsersController(UserManager<ApplicationUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        [HttpGet("current")]
+        public async Task<IActionResult> GetCurrentUserUserName()
+        {
+            var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (currentUser == null)
+            {
+                return BadRequest(new HttpResponseHandler
+                {
+                    Message = $"You are not logged in.",
+                    StatusCode = HttpStatusCode.BadRequest,
+                });
+            }
+
+            var userDto = _mapper.Map<UserDto>(currentUser);
+
+            return Ok(userDto);
+        }
+    }
+}
